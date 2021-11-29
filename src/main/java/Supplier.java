4197f1c3e0afd5f80ad83318bd58c0ef11e856ec
golde <goldeanu_klau_clau@yahoo.co.uk>
2021-11-29 10:59:28 +0200

    change cupplier

--- src/main/java/Supplier.java
+++ src/main/java/Supplier.java
@@ -1,2 +1,26 @@
+import java.time.LocalDate;
+
 public class Supplier {
+    private double price;
+    private LocalDate date;
+
+    public boolean checkAvaibility(){
+        return date.isBefore(LocalDate.now());
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
 }
