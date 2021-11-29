c681b58521e6a9bf243546d2128f8978515bd0f1
javasile <goldeanu_klau_clau@yahoo.co.uk>
2021-11-29 11:00:40 +0200

    added Basket ProductG

--- src/main/java/GenericProduct.java
+++ src/main/java/GenericProduct.java
@@ -1,2 +1,31 @@
 public class GenericProduct {
+
+    private String name;
+    private int id;
+
+    Supplier supplier = new Supplier ();
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Supplier getSupplier() {
+        return supplier;
+    }
+
+    public void setSupplier(Supplier supplier) {
+        this.supplier = supplier;
+    }
 }
